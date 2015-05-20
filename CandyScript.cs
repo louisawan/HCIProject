051eabf7fe48f11e7141f74b81b744b9b44a1b94
louisawan <louisa.wan@nyu.edu>
2015-05-19 22:07:34 -0400

    Character Script
    This is the script for the character.

---
+++ CandyScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CandyScript : MonoBehaviour {
+	TextMesh score;
+
+	// Use this for initialization
+	void Start () {
+		score = GameObject.FindGameObjectWithTag ("score").GetComponent<TextMesh>();
+		score.text = "SCORE: " + CharacterScript.totalScore;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider other)
+	{	
+		Destroy (this.gameObject);
+		if (other.gameObject.tag == "Player") {
+			CharacterScript.totalScore+=1;
+			score.text = "SCORE: " + CharacterScript.totalScore;
+
+		}
+	}
+}
---
+++ CharacterScript.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using Uniduino;
+
+public class CharacterScript : MonoBehaviour {
+	public float thrust;
+	public Arduino arduino;
+	public int tiltSensor = 2;
+	public int tiltValue;
+	public GameObject candyPrefab;
+	public Rigidbody rb;
+	public static int totalScore=0;
+	public AudioClip impact;
+	AudioSource audio;
+	// Use this for initialization
+	void Start () {
+
+		arduino = Arduino.global;
+		arduino.Setup (ConfigurePins);
+		StartCoroutine (SpawnTimer ());
+		audio = GetComponent<AudioSource>();
+	}
+	void ConfigurePins()
+	{
+		arduino.pinMode (tiltSensor, PinMode.INPUT);
+		arduino.reportDigital((byte)(tiltSensor/8), 1);
+		//arduino.pinMode (pressureSensor, PinMode.ANALOG);
+		//arduino.reportAnalog (pin, 1);
+
+	}
+	// Update is called once per frame
+	void Update () {
+	
+
+		tiltValue = arduino.digitalRead (tiltSensor);
+		Debug.Log (tiltValue);
+		if (tiltValue==1) {
+			transform.Translate (Vector3.left * 5.0f * Time.deltaTime);
+		} else if(tiltValue==0) 
+		{
+			transform.Translate (Vector3.right * 5.0f * Time.deltaTime);
+
+		}
+
+	}
+	void SpawnCandies()
+	{
+		float randomX = Random.Range (-5.0f, 5.0f);
+		Instantiate (candyPrefab, new Vector3(randomX, 7.0f, 0.0f), Quaternion.identity);
+		StartCoroutine (SpawnTimer ());
+	}
+	IEnumerator SpawnTimer()
+	{
+		float randomTime = Random.Range(0,3);
+		yield return new WaitForSeconds (randomTime);
+		//StartCoroutine (SpawnCandies ());
+		SpawnCandies ();
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "candy") {
+			audio.PlayOneShot (impact, 1.0f);
+		}
+	}
+
+}
